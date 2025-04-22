801e32be1ed32837a02df9d5c28e785a6f88679d
Brock Bena <bbena99@gmail.com>
2025-04-21 23:25:23 -0500

    Bare bones socket.io implementation

--- server/app.js
+++ server/app.js
@@ -4,8 +4,6 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -19,8 +17,20 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Start of socket work
+const socketServer = app.listen(8080);
+const socketio = require('socket.io');
+const io = socketio(socketServer,{
+
+})
+io.on('connect',socket=>{
+  console.log(socket.id)
+})
+
+//Start of routers using port 3000
+var indexRouter = require('./routes/index');
+
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
