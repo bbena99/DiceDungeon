a459a84e390e1ae473d311585da6f095e1d8e4c7
Brock Bena <bbena99@gmail.com>
2025-04-22 00:03:26 -0500

    Removed unused secondary theme

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,7 +3,6 @@ import { ref } from "vue";
 import  NavBar  from "./components/NavBar.vue";
 
 const theme = ref(localStorage.getItem('theme')??'light');
-const secondary = ref(localStorage.getItem('Theme')??'');
 const setTheme = (newTheme:string)=>{
   localStorage.setItem('theme',newTheme);
   theme.value=newTheme;
@@ -11,7 +10,7 @@ const setTheme = (newTheme:string)=>{
 </script>
 
 <template>
-  <div :class="[theme,secondary]" class=" font-sans">
+  <div :class="theme" class=" font-sans">
     <NavBar :theme :setTheme :secondary :setSecondary/>
     <div class="w-screen bg-gradient-to-b from-bg1 to-bg2 text-text overflow-y-scroll"style="height: calc(100vh - 5rem);">
       <RouterView/>
