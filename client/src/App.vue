065abb85cefb3c3680fb49c2d004ef348436a264
Brock Bena <bbena99@gmail.com>
2025-04-26 22:30:29 -0500

    Removed previous code

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,21 +1,10 @@
 <script setup lang="ts">
-import { ref } from "vue";
-import  NavBar  from "./components/NavBar.vue";
-
-const theme = ref(localStorage.getItem('theme')??'light');
-const setTheme = (newTheme:string)=>{
-  localStorage.setItem('theme',newTheme);
-  theme.value=newTheme;
-}
 </script>
 
 <template>
-  <div :class="theme" class=" font-sans">
-    <NavBar :theme :setTheme :secondary :setSecondary/>
-    <div class="w-screen bg-gradient-to-b from-bg1 to-bg2 text-text overflow-y-scroll"style="height: calc(100vh - 5rem);">
-      <RouterView/>
-    </div>
-  </div>
+<div class="w-screen h-screen bg-gray-900">
+  <RouterView/>
+</div>
 </template>
 
 <style scoped>
